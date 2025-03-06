83acf335f8cc3fc6444e700cddcd6457282deec8
agarciar37 <92687879+agarciar37@users.noreply.github.com>
2025-03-06 17:58:53 +0100

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,12 +2,12 @@ import { Handlers } from "$fresh/server.ts";
 
 export const handler: Handlers = {
   GET(req) {
-    const baseUrl = new URL(req.url); // Obtén la base de la solicitud
-    const triviaUrl = new URL("/trivia", baseUrl); // Construye la URL completa
-    return Response.redirect(triviaUrl, 303); // Redirige a la ruta trivia
+    const baseUrl = new URL(req.url);
+    const triviaUrl = new URL("/trivia", baseUrl);
+    return Response.redirect(triviaUrl, 303);
   },
 };
 
 export default function Home() {
-  return null; // No se necesita contenido porque se redirige automáticamente.
+  return null; 
 }
